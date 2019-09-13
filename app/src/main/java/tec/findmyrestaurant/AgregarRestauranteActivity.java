e8105acabfedcf7b9d065e56fc0efa01b83819f4
Kahho Chen <kahho.chen@gmail.com>
2019-09-13 12:30:45 -0600

    agregado pantalla agregar restaurante

---
+++ app/src/main/java/tec/findmyrestaurant/AgregarRestauranteActivity.java
@@ -0,0 +1,14 @@
+package tec.findmyrestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarRestauranteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_restaurante);
+    }
+}
