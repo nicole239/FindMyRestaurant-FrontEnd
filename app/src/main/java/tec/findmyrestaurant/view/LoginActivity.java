0acabb0ee7b18d67aab93f875bddd48a5043f04d
nicole239 <nicole.carvajal.b@gmail.com>
2019-09-27 09:24:42 -0600

    SearchActivity fixed + facebook login (in process)

--- app/src/main/java/tec/findmyrestaurant/view/LoginActivity.java
+++ app/src/main/java/tec/findmyrestaurant/view/LoginActivity.java
@@ -2,6 +2,7 @@ package tec.findmyrestaurant.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,6 +21,18 @@ import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttribu
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler;
 import com.amazonaws.services.cognitoidentityprovider.model.SignUpResult;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import tec.findmyrestaurant.R;
 import tec.findmyrestaurant.api.Message;
 import tec.findmyrestaurant.api.Response;
@@ -31,12 +44,64 @@ import tec.findmyrestaurant.model.User;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private CallbackManager callbackManager;
+
     private AWSCredentialsProvider credentialsProvider;
     private AWSConfiguration configuration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.btnLoginFacebook);
+        loginButton.setPermissions("email");
+
+        // Callback registration
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                final User user = new User();
+                GraphRequest request = GraphRequest.newMeRequest(
+                        accessToken,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("LoginActivity", response.toString());
+
+                                // Application code
+                                try {
+                                    user.setEmail(object.getString("email"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "email");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+                SessionManager.saveToken(LoginActivity.this, user,accessToken.getToken());
+                Intent intent = new Intent(LoginActivity.this, TabbedActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(getApplicationContext(),exception.toString(),Toast.LENGTH_LONG).show();
+                Log.e("mierror",exception.toString());
+            }
+        });
+
+
 
        /* AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
             @Override
@@ -66,6 +131,7 @@ public class LoginActivity extends AppCompatActivity {
         final User user = new User();
         user.setEmail(txtEmail.getText().toString());
         user.setPassword(txtPassword.getText().toString());
+
         UserRequest.authUser(getApplicationContext(),user, new Response<User>(){
             @Override
             public void onSuccess(Message message) {
@@ -99,5 +165,12 @@ public class LoginActivity extends AppCompatActivity {
         CognitoSettings cognitoSettings = new CognitoSettings(this);
         cognitoSettings.getUserPool().signUpInBackground("steven","ABCabc123.",userAttributes,null,signUpHandler);
 
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/tec/findmyrestaurant/view/SearchActivity.java
+++ app/src/main/java/tec/findmyrestaurant/view/SearchActivity.java
@@ -3,19 +3,15 @@ package tec.findmyrestaurant.view;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.database.DataSetObserver;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,14 +21,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import tec.findmyrestaurant.R;
+import tec.findmyrestaurant.api.Message;
+import tec.findmyrestaurant.api.Response;
+import tec.findmyrestaurant.api.RestaurantRequest;
 import tec.findmyrestaurant.model.FoodType;
-import tec.findmyrestaurant.model.Restaurant;
 
 public class SearchActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -41,7 +37,6 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
     EditText txtRadio;
     Spinner spinnerType;
     RatingBar ratingBarcalification;
-    List<FoodType> foodTypes;
     private GoogleMap mMap;
     private MapView mapView;
     Marker locationMarker;
@@ -63,8 +58,9 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
         txtName = findViewById(R.id.txtSearchName);
         txtRadio = findViewById(R.id.txtRadio);
         ratingBarcalification = findViewById(R.id.searchCalification);
+        ratingBarcalification.setRating(3f);
         spinnerPrice = findViewById(R.id.spinnerPrice);
-        spinnerType = findViewById(R.id.spinnerFoodType);
+        spinnerType = findViewById(R.id.spinnerSearchFoodType);
 
         setPriceSpinner();
         setFoodTypeSpinner();
@@ -74,7 +70,6 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
     public void btnSearch_onClick(View view){
         //Get values from view
         String name = txtName.getText().toString();
-        int radio = Integer.parseInt(txtRadio.getText().toString());
         char price;
         switch(spinnerPrice.getSelectedItemPosition()){
             case 0:
@@ -87,10 +82,18 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
                 price = 'H';
                 break;
         }
-        FoodType type = foodTypes.get(spinnerType.getSelectedItemPosition());
+        FoodType type = (FoodType) spinnerType.getSelectedItem();
         float minCalification = ratingBarcalification.getRating();
-        double latitude = locationMarker.getPosition().latitude;
-        double longitude = locationMarker.getPosition().longitude;
+
+        Log.i("SEARCHING",name+" "+price+ " "+type.getName()+" "+minCalification);
+
+        if(locationMarker != null) {
+            double latitude = locationMarker.getPosition().latitude;
+            double longitude = locationMarker.getPosition().longitude;
+            int radio = Integer.parseInt(txtRadio.getText().toString());
+            Log.i("SEARCHING",latitude+" ,"+longitude+" r:"+radio);
+        }
+
 
         //TODO: CALL SERVER SEARCH
     }
@@ -110,28 +113,17 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
     }
 
     private void setFoodTypeSpinner(){
-        getFoodTypes();
-        String[] types = new String[foodTypes.size()];
-        for (int i = 0; i < foodTypes.size(); i++)
-        {
-            types[i] = foodTypes.get(i).getName();
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_spinner_item,
-                types
-        );
-        spinnerType.setAdapter(adapter);
-
-
-    }
-
-    private void getFoodTypes(){
-        //TODO: GET FOOD TYPES FROM BD
-        foodTypes = new ArrayList<>();
-        foodTypes.add(new FoodType(1,"Tipica"));
-        foodTypes.add(new FoodType(4,"Chatarra"));
-        foodTypes.add(new FoodType(7,"China"));
+        RestaurantRequest.getFoodTypes(this,new Response<FoodType>(){
+            @Override
+            public void onSuccess(List<FoodType> list) {
+                ArrayAdapter<FoodType> adapterFood = new ArrayAdapter<FoodType>(SearchActivity.this,R.layout.spinner_item,list);
+                spinnerType.setAdapter(adapterFood);
+            }
+            @Override
+            public void onFailure(Message message) {
+                Toast.makeText(SearchActivity.this,"Couldn't get food type",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -144,7 +136,10 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
             @Override
             public void onMapClick(LatLng point) {
                 mMap.clear();
+                if(locationMarker == null)
+                    txtRadio.setText("10");
                 locationMarker = mMap.addMarker(new MarkerOptions().position(point).draggable(true));
+
             }
         });
 
